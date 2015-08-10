69167fb3b0d4b0dec8074bc1b0c04daa471abeb8
Stefan Larsson <steff_dj@msn.com>
2015-08-10 20:32:54 +0200

    testin av steffe

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		  int i = 0;
+		  int sum = 0;
+		  while(i<10)
+		  {
+		   
+		   sum += i;
+		   i++;
+		   System.out.println(sum);
+		  }
+		  System.out.println("The sum is:" + sum);
+	}
+
+}
